c8ab349ffc40973bcabf109d0c2dd099f252766c
recep <67recepsen67@gmail.com>
2020-09-10 22:19:48 +0300

    duzenleme

--- assets/JS/progressBar.js
+++ assets/JS/progressBar.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
         let val =$(element).data('val');
         let title =$(element).data('title');
         let color =$(element).data('color') ?? "red";
+        $(element).append(` <div class="progressBar-header"> <span class="title"></span> <span class="progressCount"></span> </div><div class="progressBar-bar"> <div class="bar"></div></div>`);
         $(element).find('.progressBar-header .title').text(title);
         $(element).find('.progressBar-header .progressCount').text(`${val}/${max}`);
         $(element).find('.progressBar-bar .bar').css('width',`${val/max*100}%`);
