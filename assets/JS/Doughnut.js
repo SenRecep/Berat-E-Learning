74dfe03eb31ef9de595cd97f729426424b2e7898
recep <67recepsen67@gmail.com>
2020-09-11 08:06:52 +0300

    nested labales

--- assets/JS/Doughnut.js
+++ assets/JS/Doughnut.js
@@ -1,13 +1,10 @@
 var totalDay = 100;
 var value = 45;
 var data = {
-    labels: [
-        `Tamamlanan Gün`,
-        ""
-    ],
     datasets: [
         {
             data: [value, totalDay - value],
+            labels:['Eğitim Sürecini Tamamlamış Personel',''],
             backgroundColor: [
                 "#f6b900",
                 "#e6e6e6"
@@ -18,7 +15,8 @@ var data = {
             ]
         },
         {
-            data: [0, totalDay],
+            data: [60, 120],
+            labels:['Eğitim Süreci Devam Eden Personel',''],
             backgroundColor: [
                 "#f6b900",
                 "#e6e6e6"
@@ -29,7 +27,8 @@ var data = {
             ]
         },
         {
-            data: [0, totalDay],
+            data: [20, 40],
+            labels:['Eğitim Sürecine Henüz Başlamamış Personel',''],
             backgroundColor: [
                 "#f6b900",
                 "#e6e6e6"
@@ -52,12 +51,18 @@ var myChart = new Chart(document.getElementById('myChart2'), {
         legend: {
             display: false
         },
+        labels: {
+            render: 'label'
+          },
         cutoutPercentage: 80,
         tooltips: {
-            filter: function (item, data) {
-                var label = data.labels[item.index];
-                if (label) return item;
+            callbacks: {
+              label: function(tooltipItem, data) {
+                var dataset = data.datasets[tooltipItem.datasetIndex];
+                var index = tooltipItem.index;
+                return dataset.labels[index] + ": " + dataset.data[index];
+              }
             }
-        }
+          }
     }
 });
